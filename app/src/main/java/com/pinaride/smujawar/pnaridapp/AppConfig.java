3de3b9ccbb0038d8022b9dcf29e2bff1866d42ba
salim-mujawar <smujawar@splunk.com>
2016-06-03 15:43:36 +0530

    Login and Registeration with API calls done

---
+++ app/src/main/java/com/pinaride/smujawar/pnaridapp/AppConfig.java
@@ -0,0 +1,12 @@
+package com.pinaride.smujawar.pnaridapp;
+
+/**
+ * Created by smujawar on 3/6/16.
+ */
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.5.223/projects/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.5.223/projects/android_login_api/register.php";
+}
