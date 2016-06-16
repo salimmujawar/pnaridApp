65c0fb0a04be0e4ef9f7f3441f9834f7ad065f6d
salimmujawar <salim685@gmail.com>
2016-06-16 17:02:20 +0530

    Sliding menu added

--- app/src/main/java/com/pinaride/smujawar/pnaridapp/AppConfig.java
+++ app/src/main/java/com/pinaride/smujawar/pnaridapp/AppConfig.java
@@ -5,8 +5,8 @@ package com.pinaride.smujawar.pnaridapp;
  */
 public class AppConfig {
     // Server user login url
-    public static String URL_LOGIN = "http://192.168.5.223/projects/android_login_api/login.php";
+    public static String URL_LOGIN = "http://192.168.5.128/projects/android_login_api/login.php";
 
     // Server user register url
-    public static String URL_REGISTER = "http://192.168.5.223/projects/android_login_api/register.php";
+    public static String URL_REGISTER = "http://192.168.5.128/projects/android_login_api/register.php";
 }
---
+++ app/src/main/java/com/pinaride/smujawar/pnaridapp/fragment/HomeFragment.java
@@ -0,0 +1,26 @@
+package com.pinaride.smujawar.pnaridapp.fragment;
+
+/**
+ * Created by root on 15/6/16.
+ */
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.pinaride.smujawar.pnaridapp.R;
+
+public class HomeFragment extends Fragment {
+
+    public HomeFragment(){}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        return rootView;
+    }
+}
